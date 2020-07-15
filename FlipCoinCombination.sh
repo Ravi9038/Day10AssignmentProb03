96a0cc1ce9dc5a21b31789e9d057608194311b8e
ravi9038 <ravijadhav9038@gmail.com>
2020-07-15 17:09:45 +0530

    Singlet Coin flip

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,11 +1,31 @@
 #!/bin/bash -x
+ 
+read -p "Enter the coin flip time : " flipTime
 
-echo "Flip Coin Simulation"
-var=$((RANDOM%2))
+i=0
+countH=0
+countT=0
 
-if [ $var -eq 1 ]
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
+declare -A flipCoinDict
+while [ $i -ne $flipTime ]
+do
+	randomTime=$(( (RANDOM%4)+1 ))
+	if [ $randomTime -eq 1 ]
+	then
+		flipCoinDict[$i]="H"
+		((countH++))
+
+	else [ $randomTime -eq 2 ]
+
+		flipCoinDict[$i]="T"
+		((countT++))
+	fi
+	((i++))
+done
+echo Dictionary Values : ${flipCoinDict[*]}
+
+singletHPer=`echo $countH $flipTime | awk -F" " '{ a=$1; b=$2; res1=(a/b*100); print res1}'`
+echo Percentage of H : $singletHPer %
+
+singletTPer=`echo $countT $flipTime | awk -F" " '{ a=$1; b=$2; res1=(a/b*100); print res1}'`
+echo Percentage of T : $singletTPer %
