860ba7499f77d2a39368cdeaa14574f437ae0391
ravi9038 <ravijadhav9038@gmail.com>
2020-07-15 17:18:22 +0530

    Triplet File Combination

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -76,3 +76,83 @@ echo Percentage of TH : $dupletTHPer %
 
 dupletTTPer=`echo $countTT $flipTimeDub | awk -F" " '{ a=$1; b=$2; res1=(a/b*100); print res1}'`
 echo Percentage of TT : $dupletTTPer %
+
+#Triplet combination
+
+read -p "Enter the coin flip time : " flipTimeT
+
+i=0
+countHHH=0
+countHHT=0
+countHTH=0
+countHTT=0
+countTHH=0
+countTHT=0
+countTTH=0
+countTTT=0
+
+declare -A flipCoinDict
+while [ $i -ne $flipTimeT ]
+do
+	randomTime=$(( (RANDOM%8)+1 ))
+	if [ $randomTime -eq 1 ]
+	then
+		flipCoinDict[$i]="HHH"
+		((countHHH++))
+
+	elif [ $randomTime -eq 2 ]
+	then
+		flipCoinDict[$i]="HHT"
+		((countHHT++))
+	elif [ $randomTime -eq 3 ]
+	then
+		flipCoinDict[$i]="HTH"
+		((countHTH++))
+	elif [ $randomTime -eq 4 ]
+	then
+		flipCoinDict[$i]="HTT"
+		((countHTT++))
+	elif [ $randomTime -eq 5 ]
+	then
+		flipCoinDict[$i]="THH"
+		((countTHH++))
+	elif [ $randomTime -eq 6 ]
+	then
+		flipCoinDict[$i]="THT"
+		((countTHT++))
+	elif [ $randomTime -eq 7 ]
+	then
+		flipCoinDict[$i]="TTH"
+		((countTTH++))
+	else
+		flipCoinDict[$i]="TTT"
+		((countTTT++))
+	fi
+
+	((i++))
+done
+echo ${flipCoinDict[*]}
+
+tripletHHHPer=`echo $countHHH $flipTimeT | awk -F" " '{ a=$1; b=$2; res1=(a/b*100); print res1}'`
+echo Percentage of HHH : $tripletHHHPer %
+
+tripletHTTPer=`echo $countHTT $flipTimeT | awk -F" " '{ a=$1; b=$2; res1=(a/b*100); print res1}'`
+echo Percentage of HHT : $tripletHTTPer %
+
+tripletHTHPer=`echo $countHTH $flipTimeT | awk -F" " '{ a=$1; b=$2; res1=(a/b*100); print res1}'`
+echo Percentage of HTH : $tripletHTHPer %
+
+tripletHTTPer=`echo $countHTT $flipTimeT | awk -F" " '{ a=$1; b=$2; res1=(a/b*100); print res1}'`
+echo Percentage of HTT : $tripletHTTPer %
+
+tripletTHHPer=`echo $countTHH $flipTimeT | awk -F" " '{ a=$1; b=$2; res1=(a/b*100); print res1}'`
+echo Percentage of THH : $tripletTHHPer %
+
+tripletTHTPer=`echo $countTHT $flipTimeT | awk -F" " '{ a=$1; b=$2; res1=(a/b*100); print res1}'`
+echo Percentage of THT : $tripletTHTPer %
+
+tripletTHHPer=`echo $countTHH $flipTimeT | awk -F" " '{ a=$1; b=$2; res1=(a/b*100); print res1}'`
+echo Percentage of THH : $tripletTHHPer %
+
+tripletTTTPer=`echo $countTTT $flipTimeT | awk -F" " '{ a=$1; b=$2; res1=(a/b*100); print res1}'`
+echo Percentage of TTT : $tripletTTTPer %
